2095f01f2a392373ccfc117f911cdf6fda7ed15e
Pratik <thakurpratik199@gmail.com>
2020-02-24 21:37:31 +0530

    message

---
+++ myproject/fake_model.py
@@ -0,0 +1,40 @@
+import csv
+def fake_predict(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,ab,ac,ad):
+    import pickle
+    x=[[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,ab,ac,ad]]
+    randomforest=pickle.load(open('myproject/model.sav','rb'))
+    prediction=randomforest.predict(x)
+    if y==1:
+        prediction='Rabies'
+    elif u==1:
+        prediction='Chicken Pox'
+    elif o==1:
+        prediction="Measales"
+    elif ad==1:
+        prediction="Conjuctivities"    
+    elif prediction==0:
+        prediction="Anaemia"
+    elif prediction==1:
+        prediction="Asthma"    
+    elif prediction==4:
+        prediction="Common Cold" 
+    elif prediction==11:
+        prediction="Diarrohea"       
+    elif prediction==6:
+        prediction="FLU" 
+    elif prediction==12:
+        prediction="Hepatatis A" 
+    elif prediction==13:
+        prediction="Malaria" 
+    elif prediction==8:
+        prediction="Measales" 
+    elif prediction==10:
+        prediction="TB" 
+    elif prediction==14:
+        prediction="Typhoid" 
+    elif prediction==7:
+        prediction="Influenza" 
+    elif prediction==3:
+        prediction="Cholera" 
+                                              
+    return prediction
